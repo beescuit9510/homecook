34a8c5e2f2aa380bbcd93304587c0d320e138998
pakj1 <pakj1@DESKTOP-O6UGQ62>
2021-12-02 17:51:32 +0900

    manageMenu 페이지 추가

--- src/main/java/kr/or/delivery/seller/controller/DeliverySellerController.java
+++ src/main/java/kr/or/delivery/seller/controller/DeliverySellerController.java
@@ -7,9 +7,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DeliverySellerController {
 
 	@RequestMapping(value = "/manageMarket.do")
-	public String deliverySeller()
+	public String manageMarket()
 	{
 		return "delivery/seller/manageMarket";
+	}
+	
+	@RequestMapping(value = "/manageMenu.do")
+	public String manageMenu()
+	{
+		return "delivery/seller/manageMenu";
 	}
 	
 }
